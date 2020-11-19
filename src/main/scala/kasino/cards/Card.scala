b0792e5208d6d0807fe243c95da03b0f5cc374cd
Sune Precht Reeh <Sune@Reeh.dk>
2020-11-19 13:02:36 +0100

    Updated Card docstrings

--- src/main/scala/kasino/cards/Card.scala
+++ src/main/scala/kasino/cards/Card.scala
@@ -1,37 +1,67 @@
 package kasino.cards
 
+/**
+ *  Trait for cards that can be used in a game of "Nørdekasino".
+ */
 trait Card {
+
   /**
-   * @return The companion object of the class implementing Card
+   * @return The companion object of the class implementing [[Card]].
    */
   val companion: CardCompanion[Card]
   
   /**
-   * @return The set of possible values that the Card can be used as.
+   * @return The set of possible values that this [[Card]] can be used as.
    */
   def values: Set[Int]
 
   /**
-   * @return How many points the Card is worth.
+   * @return How many points this [[Card]] is worth.
    */
   def points: Int
-  
+
+  /**
+   * @return [[true]] if this [[Card]] is the Five of Spades (or equivalent card).
+   */
   def isFiveOfSpades: Boolean
-  
+
+  /**
+   * @return [[true]] if this [[Card]] is Spades (or equivalent suit).
+   */
   def isSpades: Boolean
   
+  /**
+   * @return Optional flavor description of this [[Card]].
+   */
   def description: Option[String] = None
 }
 
 /**
- * Trait required to be implemented by a companion object associated to any class implemeting Card.
+ * Trait required to be implemented by a companion object associated to any class implemeting [[Card]].
  * 
- * @tparam C The class implementing Card
+ * @tparam C a class implementing [[Card]]
  */
 trait CardCompanion[+C <: Card] {
-  def apply(string: String): C = stringToCard(string)
+  /**
+   * Creates a [[C]] from its name.
+   * 
+   * @param name the name of the [[C]] to create
+   * @return a new [[C]] representing the card with the given name.
+   */
+  def apply(name: String): C = stringToCard(name)
+
+  /**
+   * Creates a [[C]] from its name.
+   * 
+   * @param name the name of the [[C]] to create
+   * @return a new [[C]] representing the card with the given name.
+   */
+  def stringToCard(name: String): C
   
-  def stringToCard(string: String): C
-  
+  /**
+   * Creates a new default deck populated with cards of type [[C]]. There is no guarentee that the deck is randomized, so the user should shuffle the deck themself before playing.
+   * 
+   * @return a new default [[C]] deck
+   */
   def newDeck: Seq[C]
 }
