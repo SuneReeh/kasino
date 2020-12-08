d61f6996ab09a06fea16861fd21f078b76fdc96c
Sune Precht Reeh <Sune@Reeh.dk>
2020-12-08 13:24:37 +0100

    Added handle for deck.size to Player constructor.

--- src/main/scala/kasino/game/Game.scala
+++ src/main/scala/kasino/game/Game.scala
@@ -7,6 +7,9 @@ import scala.collection.mutable.{ArrayDeque, Map, Queue}
 
 /**
  * A concrete game of "Nørdekasino".
+ * 
+ * @param controllers controllers for the [[Player]]s in this game.
+ * @param newDeck a deck of [[kasino.cards.Card]]s with which to play a game.
  */
 class Game (controllers: Iterable[Controller], newDeck: Iterable[Card]) {
   private val deck: ArrayDeque[Card] = Queue(scala.util.Random.shuffle(newDeck).toSeq: _*)
@@ -15,7 +18,7 @@ class Game (controllers: Iterable[Controller], newDeck: Iterable[Card]) {
   private val players: scala.collection.immutable.Seq[Player] =
     controllers.map{c =>
       val hand: mutable.ArrayDeque[Card] = ArrayDeque();
-      val player: Player = new Player(c, hand.view, table.view)
+      val player: Player = new Player(c, hand.view, table.view, deck.size)
       hands.addOne(player, hand)
       player
     }.toSeq
--- src/main/scala/kasino/game/Player.scala
+++ src/main/scala/kasino/game/Player.scala
@@ -4,7 +4,10 @@ import kasino.cards.Card
 import java.util.UUID
 import scala.collection.SeqView
 
-class Player (private val controller: Controller, private val handView: SeqView[Card], private val tableView: SeqView[CardStack]) {
+class Player (private val controller: Controller, 
+              private val handView: SeqView[Card], 
+              private val tableView: SeqView[CardStack],
+              deckSize: =>Int) {
   def name: String = controller.name
 
   val id : UUID = controller.id
