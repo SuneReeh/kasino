e0322570c7c665003dfcf9d94fee038acd2d1861
Sune Precht Reeh <Sune@Reeh.dk>
2020-12-09 10:42:29 +0100

    Added more fields to Game

--- src/main/scala/kasino/game/Game.scala
+++ src/main/scala/kasino/game/Game.scala
@@ -2,6 +2,7 @@ package kasino.game
 
 import kasino.cards.Card
 
+import java.util.UUID
 import scala.collection.mutable
 import scala.collection.mutable.{ArrayDeque, Map, Queue}
 
@@ -13,13 +14,35 @@ import scala.collection.mutable.{ArrayDeque, Map, Queue}
  */
 class Game (controllers: Iterable[Controller], newDeck: Iterable[Card]) {
   private val deck: ArrayDeque[Card] = Queue(scala.util.Random.shuffle(newDeck).toSeq: _*)
-  private val table: ArrayDeque[CardStack] = ArrayDeque();
-  private val hands: Map[Player, ArrayDeque[Card]] = Map.empty;
+  private val table: ArrayDeque[CardStack] = ArrayDeque()
+  private val hands: Map[Player, ArrayDeque[Card]] = Map.empty
   private val players: scala.collection.immutable.Seq[Player] =
-    controllers.map{c =>
-      val hand: mutable.ArrayDeque[Card] = ArrayDeque();
-      val player: Player = new Player(c, hand.view, table.view, deck.size)
+    scala.util.Random.shuffle(controllers).map{c =>
+      val hand: mutable.ArrayDeque[Card] = ArrayDeque()
+      val player: Player = new Player(c, hand.view, table.view, deckSize)
       hands.addOne(player, hand)
       player
     }.toSeq
+  private val numPlayers = players.size
+  private var currentPlayerPos: Int = 0
+  private var lastToClaimPos: Option[Int] = None
+  private val cardsToClaim: ArrayDeque[Card] = ArrayDeque()
+  private var usedCard: Option[Card] = None
+
+  //Backup game state from the beginning of each turn 
+  private var tableBackup: ArrayDeque[CardStack] = ArrayDeque()
+  private var lastToClaimPosBackup: Option[Int] = None
+  
+  //End of game
+  private var gameFinished: Boolean = false;
+  //private val scores: Map[Player,???] = Map.empty
+  
+  /** The number of [[kasino.cards.Card]]s remaining in the deck. */
+  def deckSize : Int = deck.size
+  
+  /** The [[java.util.UUID]] of the current [[Player]]. */
+  def currentPlayerId: UUID = players(currentPlayerPos).id
+  
+  /** The name of the current [[Player]]. */
+  def currentPlayerName: String = players(currentPlayerPos).name
 }
