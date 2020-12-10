4816441f5f5b2fd359f7cf9e021126cba71672d3
Sune Precht Reeh <Sune@Reeh.dk>
2020-12-10 12:43:19 +0100

    Added PlayerActions stubs to Game

--- src/main/scala/kasino/game/Game.scala
+++ src/main/scala/kasino/game/Game.scala
@@ -1,11 +1,14 @@
 package kasino.game
 
 import kasino.cards.Card
+import kasino.game.Game.{ActionProvider, CardPosition, Action}
+import kasino.game.Game.CardPosition._
+
 import reeh.math.BigRational
 
 import java.util.UUID
 import scala.collection.mutable.{ArrayDeque, Map, Queue}
-import scala.util.{Try, Success, Failure}
+import scala.util.{Failure, Success, Try}
 
 /**
  * A concrete game of "Nørdekasino".
@@ -16,12 +19,12 @@ import scala.util.{Try, Success, Failure}
 class Game (controllers: Iterable[Controller], newDeck: Iterable[Card]) {
   private val deck: ArrayDeque[Card] = Queue(scala.util.Random.shuffle(newDeck).toSeq: _*)
   private val table: ArrayDeque[CardStack] = ArrayDeque()
-  private val hands: Map[Player, ArrayDeque[Card]] = Map.empty
+  private val hands: Map[UUID, ArrayDeque[Card]] = Map.empty
   private val players: scala.collection.immutable.Seq[Player] =
     scala.util.Random.shuffle(controllers).map{c =>
       val hand: ArrayDeque[Card] = ArrayDeque()
-      val player: Player = new Player(c, hand.view, table.view, deckSize, ???)
-      hands.addOne(player, hand)
+      val player: Player = new Player(c, hand.view, table.view, deckSize, generatePlayerActions(c.id))
+      hands.addOne(player.id, hand)
       player
     }.toSeq
   private val numPlayers = players.size
@@ -37,7 +40,7 @@ class Game (controllers: Iterable[Controller], newDeck: Iterable[Card]) {
   //End of game
   private var _gameFinished: Boolean = false
   def gameFinished: Boolean = _gameFinished
-  private val scores: Map[Player,BigRational] = Map.empty
+  private val scores: Map[UUID,BigRational] = Map.empty
   private var _resultReport: Option[String] = None
   def resultReport: Option[String] = _resultReport
   
@@ -49,6 +52,24 @@ class Game (controllers: Iterable[Controller], newDeck: Iterable[Card]) {
   
   /** The name of the current [[Player]]. */
   def currentPlayerName: String = players(currentPlayerPos).name
+  
+  private def generatePlayerActions(playerId: UUID): ActionProvider = new ActionProvider {
+    override def Play(posHand: Hand): Game.Action = ???
+
+    override def Add(pos1: Game.CardPosition, pos2: Game.CardPosition, res: Option[Int]): Game.Action = ???
+
+    override def Mod(pos1: Game.CardPosition, pos2: Game.CardPosition, res: Option[Int]): Game.Action = ???
+
+    override def Combine(pos1: Game.CardPosition, pos2: Game.CardPosition, res: Option[Int]): Game.Action = ???
+
+    override def Take(posTable: Table, posHand: Hand): Game.Action = ???
+
+    override def FiveOfSpades(posHand: Hand): Game.Action = ???
+
+    override def Reset: Game.Action = ???
+
+    override def End: Game.Action = ???
+  }
 }
 
 object Game {
@@ -61,7 +82,6 @@ object Game {
     def apply(): Try[Unit]
   }
   
-  import CardPosition._
   
   sealed trait ActionProvider {
     def Play(posHand : Hand): Action
