95b432b48d07bf0a2566f1434a8943254f67effc
Sune Precht Reeh <Sune@Reeh.dk>
2020-12-13 16:29:27 +0100

    Implemented Player methods

--- src/main/scala/kasino/game/Controller.scala
+++ src/main/scala/kasino/game/Controller.scala
@@ -1,11 +1,24 @@
 package kasino.game
 
+import kasino.cards.Card
+import kasino.exceptions.KasinoException
+
 import java.util.UUID
+import scala.collection.SeqView
+import scala.util.{Try, Success, Failure}
 
 trait Controller {
   val id : UUID = UUID.randomUUID()
 
   private var _name : String = ""
   def name: String = _name
-  private def name_=(name: String): Unit = {_name = name}
+  protected def name_=(name: String): Unit = {_name = name}
+  
+  def updateGameState(handView: SeqView[Card], tableView: SeqView[CardStack], deckSize: Int, currentPlayerId: UUID, currentPlayerName: String): Unit
+  
+  def getReady(): Boolean 
+  
+  def getAction(): Player.Action 
+  
+  def reportFailure(failed: Failure[KasinoException]): Unit
 }
--- src/main/scala/kasino/game/Player.scala
+++ src/main/scala/kasino/game/Player.scala
@@ -1,12 +1,13 @@
 package kasino.game
 
 import kasino.cards.Card
+import kasino.exceptions.KasinoException
 import kasino.game.Game.{Action, ActionProvider, CardPosition}
 import kasino.game.Game.CardPosition._
 
 import java.util.UUID
 import scala.collection.SeqView
-import scala.util.{Try, Success, Failure}
+import scala.util.{Failure, Success, Try}
 
 class Player (private val controller: Controller, 
               private val handView: SeqView[Card], 
@@ -28,7 +29,32 @@ class Player (private val controller: Controller,
   private def reset(): Try[Unit] = actions.reset()
   private def end(): Try[Unit] = actions.end()
   
-  def takeTurn(): Unit = ???
+  def updateGameState(): Unit = {
+    controller.updateGameState(handView, tableView, deckSize, currentPlayerId, currentPlayerName)
+  }
+  
+  def takeTurn(): Unit = {
+    import Player.Action._
+    while {
+      val action = controller.getAction()
+      val attempt: Try[Unit] = action match {
+        case Play(posHand: Hand) => play(posHand)
+        case Add(pos1: CardPosition, pos2: CardPosition, res) => add(pos1, pos2, res)
+        case Mod(pos1: CardPosition, pos2: CardPosition, res) => mod(pos1, pos2, res)
+        case Combine(pos1: CardPosition, pos2: CardPosition, res) => combine(pos1, pos2, res)
+        case Take(posTable: Table, posHand: Hand) => take(posTable, posHand)
+        case FiveOfSpades(posHand : Hand) => fiveOfSpades(posHand)
+        case Reset => reset()
+        case End => end()
+      }
+      attempt match {
+        case Failure(exception: KasinoException) => controller.reportFailure(Failure(exception))
+        case Failure(otherException) => otherException.printStackTrace()
+        case _ => ()
+      }
+      action == End && attempt.isSuccess
+    } do ()
+  }
 }
 
 object Player {
